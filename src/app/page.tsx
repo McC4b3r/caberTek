097f436de760e70ecc6504cf4f0ca8ce2df53aa3
Ryan McCabe <ryan.mccabe089@gmail.com>
2023-06-19 16:58:30 -0700

    set up chakra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import * as React from 'react'
+import { Box } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <div>
+    <Box>
       hi
-    </div>
+    </Box>
   )
 }
